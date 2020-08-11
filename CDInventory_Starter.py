b368e3b96812b097f665eb0ad190b6cbc08d1411
Rupa Guha <guhasri@hotmail.com>
2020-08-10 17:41:12 -0700

    Adding a Python file
    Adding a Python file

---
+++ CDInventory_Starter.py
@@ -0,0 +1,18 @@
+#------------------------------------------#
+# Title: CDInventory.py
+# Desc: Script CDINventory to store CD Inventory data
+# Change Log: (Who, When, What)
+# DBiesinger, 2030-Jan-01, Created File
+#------------------------------------------#
+
+# 1. Display menu allowing the user to choose: 'Add CD', 'Display Current Inventory', 'Save Inventory to file' and 'exit'
+
+# 2. Add data to the table (2d-list) each time the user wants to add data
+
+# 3. Display the current data to the user each time the user wants to display the data
+
+# 4. Save the data to a text file CDInventory.txt if the user chooses so
+
+# 5. Exit the program if the user chooses so.
+
+
